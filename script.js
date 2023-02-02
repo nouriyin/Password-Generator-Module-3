796c775f00181f319f5cfb7ba12fe22f3d1414d0
nouriyin <courtneyy.noreen@gmail.com>
2023-02-02 00:38:12 -0500

    generates basic password

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ var pwdCriteria = {
   maxLength: 128,
 
 }
+var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+var specialCharacters = [".","*","?","!"]
 function generatePassword() {
+  var password = ""
   var hasSpecialCharacter = false
   var passwordLength = window.prompt("How long is your password?")
   var includesLowercase = window.prompt("Do you want to include lowercase letters?")
   var includesUppercase = window.prompt("Do you want to include uppercase letters?")
   var includesNumbers = window.prompt("What about numbers?")
   var includesSpecialCharacters = window.prompt("Are you adding any special characters?")
-for(var x = 0; x === passwordLength; x++) {
-  
+for(var x = 0; x <= passwordLength; x++) {
+password = password + characters[Math.floor(Math.random() * (26 - 0 + 1) + 0)]
+console.log(password)
 }
+return password
 }
 // Write password to the #password input
 function writePassword() {
